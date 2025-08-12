7a6457661fe3d9f5f97734b53a0ebd19a780d8df
Rick <rickadomingues@gmail.com>
2025-08-11 22:16:33 -0300

    onde deu para chegar

--- app/src/main/java/riqinho/com/fundamentos/MainActivity.kt
+++ app/src/main/java/riqinho/com/fundamentos/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.Spring
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,28 +21,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             FundamentosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Inicial(Modifier.padding(innerPadding))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun TextCustomizado(texto: String, modifier: Modifier = Modifier) {
+        Text(
+            text = texto,
+            modifier = modifier
+        )
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FundamentosTheme {
-        Greeting("Android")
+    @Composable
+    fun Inicial(modifier: Modifier = Modifier) {
+        TextCustomizado(
+            "Primeira função composable",
+            modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewFuncaoInicial() {
+        FundamentosTheme {
+            Inicial()
+        }
     }
 }
